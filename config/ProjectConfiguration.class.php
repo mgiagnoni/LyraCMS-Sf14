49d6326753a439f46f51e7212c6c7b2b84c8589a
Massimo Giagnoni <admin@lyra-cms.com>
2009-09-30 11:50:50 +0000

    Activated plugin Doctrine and created database schema
    git-svn-id: http://lyra-cms.googlecode.com/svn/trunk@5 0e8bdcba-a4b3-11de-b1fe-bba1d6bec496

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,7 +7,7 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
+    $this->enablePlugins(array('sfDoctrinePlugin'));
+    $this->disablePlugins(array('sfPropelPlugin'));
   }
 }
