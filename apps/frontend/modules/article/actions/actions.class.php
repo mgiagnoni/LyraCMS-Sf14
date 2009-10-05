3d56811c0df91b2fde0caf7a30e36be2a1f28e23
Massimo Giagnoni <admin@lyra-cms.com>
2009-10-05 16:20:01 +0000

    Created article module, personalized global layout
    git-svn-id: http://lyra-cms.googlecode.com/svn/trunk@8 0e8bdcba-a4b3-11de-b1fe-bba1d6bec496

---
+++ apps/frontend/modules/article/actions/actions.class.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * article actions.
+ *
+ * @package    lyra
+ * @subpackage article
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class articleActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->lyra_article_list = Doctrine::getTable('LyraArticle')
+      ->createQuery('a')
+      ->execute();
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+    $this->lyra_article = Doctrine::getTable('LyraArticle')->find($request->getParameter('id'));
+    $this->forward404Unless($this->lyra_article);
+  }
+
+  public function executeNew(sfWebRequest $request)
+  {
+    $this->form = new LyraArticleForm();
+  }
+
+  public function executeCreate(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->isMethod('post'));
+
+    $this->form = new LyraArticleForm();
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('new');
+  }
+
+  public function executeEdit(sfWebRequest $request)
+  {
+    $this->forward404Unless($lyra_article = Doctrine::getTable('LyraArticle')->find($request->getParameter('id')), sprintf('Object lyra_article does not exist (%s).', $request->getParameter('id')));
+    $this->form = new LyraArticleForm($lyra_article);
+  }
+
+  public function executeUpdate(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->isMethod('post') || $request->isMethod('put'));
+    $this->forward404Unless($lyra_article = Doctrine::getTable('LyraArticle')->find($request->getParameter('id')), sprintf('Object lyra_article does not exist (%s).', $request->getParameter('id')));
+    $this->form = new LyraArticleForm($lyra_article);
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('edit');
+  }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $this->forward404Unless($lyra_article = Doctrine::getTable('LyraArticle')->find($request->getParameter('id')), sprintf('Object lyra_article does not exist (%s).', $request->getParameter('id')));
+    $lyra_article->delete();
+
+    $this->redirect('article/index');
+  }
+
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()));
+    if ($form->isValid())
+    {
+      $lyra_article = $form->save();
+
+      $this->redirect('article/edit?id='.$lyra_article->getId());
+    }
+  }
+}
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,12 +1,59 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php use_stylesheet('main.css') ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="wrapper">
+      <div id="content">
+        <div id="top">
+          <div id="logo">
+            <h1>Logo</h1>
+            <h4>Slogan</h4>
+          </div>
+          <div id="links">
+            <ul>
+              <li><?php echo link_to('Home','@homepage');?></li>
+              <li><a href="#">Articoli</a></li>
+              <li><a href="#">Contattaci</a></li>
+            </ul>
+          </div>
+        </div>
+        <div id="header">
+          <h3>Titolo pagina</h3>
+        </div>
+        <div id="contentarea">
+          <div id="leftbar">
+            <?php if ($sf_user->hasFlash('notice')): ?>
+            <div class="flash_notice">
+              <?php echo __($sf_user->getFlash('notice')) ?>
+            </div>
+            <?php endif; ?>
+
+            <?php if ($sf_user->hasFlash('error')): ?>
+            <div class="flash_error">
+              <?php echo __($sf_user->getFlash('error')) ?>
+            </div>
+            <?php endif; ?>
+            <?php echo $sf_content ?>
+          </div>
+          <div id="rightbar">
+            <h4>Menu</h4>
+          </div>
+        </div>
+        <div id="footer">
+          <div id="lyra-powered">
+            <a href="#">Powered by Lyra</a>
+          </div>
+          <div id="validator">
+          <p>Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
