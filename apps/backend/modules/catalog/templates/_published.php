ba79f5a43ef053d392bf1b513de37ebf24e951b1
Massimo Giagnoni <admin@lyra-cms.com>
2010-02-22 22:26:25 +0000

    Added publish/unpublish object actions to backend routes of catalog and comment modules.
    git-svn-id: http://lyra-cms.googlecode.com/svn/trunk@42 0e8bdcba-a4b3-11de-b1fe-bba1d6bec496

--- apps/backend/modules/catalog/templates/_published.php
+++ apps/backend/modules/catalog/templates/_published.php
@@ -1,5 +1,5 @@
 <?php if ($lyra_catalog->getIsActive()): ?>
-  <?php echo link_to(image_tag('backend/yes.png', array('alt' => __('LINK_T_PUBLISHED'))),'catalog/unpublish?id='.$lyra_catalog->getId(), array('title' => __('LINK_T_PUBLISHED'))) ?>
+  <?php echo link_to(image_tag('backend/yes.png', array('alt' => __('LINK_T_PUBLISHED'))),'lyra_catalog_unpublish', $lyra_catalog, array('title' => __('LINK_T_PUBLISHED'))) ?>
 <?php else: ?>
-  <?php echo link_to(image_tag('backend/no.png', array('alt' => __('LINK_T_UNPUBLISHED'))),'catalog/publish?id='.$lyra_catalog->getId(), array('title' => __('LINK_T_UNPUBLISHED'))) ?>
+  <?php echo link_to(image_tag('backend/no.png', array('alt' => __('LINK_T_UNPUBLISHED'))),'lyra_catalog_publish', $lyra_catalog, array('title' => __('LINK_T_UNPUBLISHED'))) ?>
 <?php endif; ?>
--- apps/backend/modules/comment/templates/_published.php
+++ apps/backend/modules/comment/templates/_published.php
@@ -1,5 +1,5 @@
 <?php if ($lyra_comment->getIsActive()): ?>
-  <?php echo link_to(image_tag('backend/yes.png', array('alt' => __('LINK_T_PUBLISHED'))),'comment/unpublish?id='.$lyra_comment->getId(), array('title' => __('LINK_T_PUBLISHED'))) ?>
+  <?php echo link_to(image_tag('backend/yes.png', array('alt' => __('LINK_T_PUBLISHED'))),'lyra_comment_unpublish', $lyra_comment, array('title' => __('LINK_T_PUBLISHED'))) ?>
 <?php else: ?>
-  <?php echo link_to(image_tag('backend/no.png', array('alt' => __('LINK_T_UNPUBLISHED'))),'comment/publish?id='.$lyra_comment->getId(), array('title' => __('LINK_T_UNPUBLISHED'))) ?>
+  <?php echo link_to(image_tag('backend/no.png', array('alt' => __('LINK_T_UNPUBLISHED'))),'lyra_comment_publish', $lyra_comment, array('title' => __('LINK_T_UNPUBLISHED'))) ?>
 <?php endif; ?>
