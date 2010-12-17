aa08c1a77fcfebd3c66e0e44b2b457edb6eee1a7
mgiagnoni <admin@lyra-cms.com>
2010-12-17 11:29:00 +0100

    Added parameter to set what user information is displayed on login component.

--- apps/frontend/modules/user/templates/_login.php
+++ apps/frontend/modules/user/templates/_login.php
@@ -1,7 +1,7 @@
 <h4><?php echo __($sf_user->isAuthenticated() ? 'HEAD_USER' : 'HEAD_LOGIN');?></h4>
 <div id="login-form-wrapper-side">
 <?php if ($sf_user->isAuthenticated()):?>
-  <?php include_partial('user/signout'); ?>
+  <?php include_partial('user/signout', array('params' => $params)); ?>
 <?php else: ?>
   <?php include_partial('user/signin', array('form' => $form, 'show_registration_link' => $show_registration_link)); ?>
 <?php endif;?>
--- apps/frontend/modules/user/templates/_signout.php
+++ apps/frontend/modules/user/templates/_signout.php
@@ -1,4 +1,21 @@
 <div class="login-info">
-<?php echo __('SIGNEDIN_AS');?>: <span class="username"><?php echo $sf_user->getGuardUser()->getUsername(); ?></span>
+<?php echo __('SIGNEDIN_AS');?>:
+<span class="username">
+<?php
+switch($params->get('user_info'))
+{
+  case 'first_name':
+    echo $sf_user->getGuardUser()->getProfile()->getFirstName();
+    break;
+  case 'first_last':
+    echo $sf_user->getGuardUser()->getProfile()->getFirstName(), ' ',
+    $sf_user->getGuardUser()->getProfile()->getLastName();
+    break;
+  default:
+    echo $sf_user->getGuardUser()->getUsername();
+    break;
+}
+?>
+</span>
 </div>
 <?php echo link_to(__('LINK_LOGOUT'), '@user_signout', array('class' => 'logout')); ?>
